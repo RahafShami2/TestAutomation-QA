9880dcf2d882063362cfaacc48a8b25958312817
eleenkmail <79339710+eleenkmail@users.noreply.github.com>
2022-04-28 02:30:38 +0300

    PreCondition(Login)

--- cypress/integration/Test_Automation.spec.js
+++ cypress/integration/Test_Automation.spec.js
@@ -4,3 +4,17 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+describe('My First Test', () => {
+  it('Visits the Kitchen Sink', () => {
+      
+cy.visit('https://skillsmatch.mdx.ac.uk/accounts/login/?next=/en/')
+      
+const username = 'blabla'
+const password = '123123'
+cy.get('[id=username]').type(`${username}`)
+cy.get('[id=password]').type(`${password}`)
+cy.get('[value=Login]').click()
+
+  })
+ })
+      
